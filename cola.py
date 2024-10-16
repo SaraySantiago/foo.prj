a0796909430470b0fced3ca853cec1c312bf75bd
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-16 12:36:31 +0200

    .

--- cola.py
+++ cola.py
@@ -1,7 +1,42 @@
 class cola:
-    pass
+    
+    __data = []
+    __size = None
+    
+    def __init__(self, size):
+        self.__size = size
+        
+    def h(self) -> int:
+        return len(self.__data)
+        
+    def __str__ (self) -> str:
+            answ = "<"
+            answ += f"{self.h()} de {self.__size}, {self.__data}"
+           # if self.esVacia(): answ += "VACIA"
+          #  if self.esLlena(): answ += "LLENA"
+            answ += ">"
+            return answ
+            
+    def esVacia(self) -> bool: 
+        pass 
+    def esLLena(self) -> bool: 
+       return False
+            
+    def enqueue(self ,something):
+         if not self.esLLena():
+            self.__data.append(something)
+    
+   #  def dqueue () -> object:
+    #     pass
 
 if __name__ == "__main__":
     
-    c = cola()
+    c = cola(4)
     print(c)
+    
+    c.enqueue (11)
+    print (c)
+    c.enqueue (22)
+    print (c)
+    c.enqueue (33)
+    print (c)
